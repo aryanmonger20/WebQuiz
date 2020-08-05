e10cccd1ece721d850f3916b279010b12a8e5c36
aryanmonger20 <56786156+aryanmonger20@users.noreply.github.com>
2020-08-05 22:25:01 +0530

    Updates

---
+++ precache-manifest.2e5888acbfb10c25013db30148228a07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45567660e06f54d2f9f454ea00bb653",
+    "url": "/WebQuiz/index.html"
+  },
+  {
+    "revision": "aa2ac28355690f533057",
+    "url": "/WebQuiz/static/css/main.7eed6213.chunk.css"
+  },
+  {
+    "revision": "b316af3a1e0f42c7f0bd",
+    "url": "/WebQuiz/static/js/2.911d2651.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WebQuiz/static/js/2.911d2651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa2ac28355690f533057",
+    "url": "/WebQuiz/static/js/main.d6ca3b9f.chunk.js"
+  },
+  {
+    "revision": "d33615fb53dfab201fa6",
+    "url": "/WebQuiz/static/js/runtime-main.bde5a14a.js"
+  }
+]);
